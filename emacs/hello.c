bd95372cafcf48828572e47331f13e20e7499f18
xieshuqiang <shuqiangxie@outlook.com>
2023-10-24 00:27:32 +0800

    2

---
+++ emacs/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+  printf("hello emacs plugins");
+  return 0;
+}
---
+++ emacs/suanfa/hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  printf("kd");
+  printf("hello gcc");
+  char *str = "123412423";
+  printf("%s", str);
+  char dest[100];
+  strcat(dest,str);
+  printf("%s", dest);
+  
+
+
+  
+  
+  
+
+  return 0;
+}
