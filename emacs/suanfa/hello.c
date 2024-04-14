2c04525fef140e344b430a3b781c42bdc04384dc
xieshuqiang <shuqiangxie@outlook.com>
2024-04-14 23:46:01 +0800

    lisp study

---
+++ emacs/suanfa/hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  int a = 100;
+  printf("kd");
+  printf("hello gcc");
+  char *str = "123412423";
+  printf("%s", str);
+  char dest[100];
+  a = 110;
+  strcat(dest,str);
+  printf("%s", dest);
+  
+
+
+  
+  
+  
+
+  return 0;
+}
