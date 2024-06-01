ed02315d6cb4874a6582bd690372e785fb88186f
xieshuqiang <shuqiangxie@outlook.com>
2024-06-01 13:49:30 +0800

    update

---
+++ emacs/testProgram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int testMethod();
+
+void test2();
+
+int main() {
+
+  int aix = 3;
+  printf("hello %d \n", aix);
+}
+
+int testMethod() { return 3 + 4; }
