61399c2465308c81e1f97ba57ecf5cd95cf2c09f
xieshuqiang <shuqiangxie@outlook.com>
2024-06-27 23:40:59 +0800

    lianxi

--- lisp/2-1有理数表示.el
+++ lisp/2-1有理数表示.el
@@ -1,12 +1,19 @@
 
 ;;有理数的表示
-
-
+(defun GCD (a b)
+  "最大公约数的计算"
+    (if (= 0 b)
+	a
+	(GCD b (% a b))))
 
 ;; 
 (defun make-rat (n d)
   "Create a rational number as a cons cell."
   (cons n d))
+(defun make-rat (n d)
+  "最简形式的分数"
+  (let ((g (GCD n d)))
+   (cons (/ n g) (/ d g))))
 
 (defun numer (x)
   "Return the numerator of the rational number."
@@ -23,9 +30,11 @@
   (princ "/")
   (princ (denom x)))
 
-(setq one-half (make-rat 1 2))  ; 创建一个分数 1/2
+(setq one-half (make-rat 4 16))  ; 创建一个分数 1/2
 (print-rat one-half
+
 )
+
 
 
 
